f40abb2760e69afe21ab5f74306786b587ab37fb
Az1tchhh <azamatbazarbai007@gmail.com>
2023-11-18 16:00:42 +0600

    third commit

--- target/classes/static/js/main.js
+++ target/classes/static/js/main.js
@@ -7,10 +7,8 @@ var messageForm = document.querySelector('#messageForm');
 var messageInput = document.querySelector('#message');
 var messageArea = document.querySelector('#messageArea');
 var connectingElement = document.querySelector('.connecting');
-
 var stompClient = null;
 var username = null;
-
 var colors = [
     '#2196F3', '#32c787', '#00BCD4', '#ff5652',
     '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
@@ -20,10 +18,10 @@ function connect(event) {
     username = document.querySelector('#name').value.trim();
 
     if(username) {
-        usernamePage.classList.add('hidden');
-        chatPage.classList.remove('hidden');
+        usernamePage.classList.add('hidden'); //clear first page
+        chatPage.classList.remove('hidden'); //open chat page
 
-        var socket = new SockJS('/ws');
+        var socket = new SockJS('/ws'); //socket
         stompClient = Stomp.over(socket);
 
         stompClient.connect({}, onConnected, onError);
@@ -34,22 +32,18 @@ function connect(event) {
 
 function onConnected() {
     // Subscribe to the Public Topic
-    stompClient.subscribe('/topic/public', onMessageReceived);
+    stompClient.subscribe('/topic/public', onMessageReceived); //listener topic
 
     // Tell your username to the server
     stompClient.send("/app/chat.addUser",
         {},
-        JSON.stringify({sender: username, type: 'JOIN'})
+        JSON.stringify({sender: username, type: 'JOIN'}) //POST stringified data
     )
 
-    connectingElement.classList.add('hidden');
+    connectingElement.classList.add('hidden'); //connected, then hide connectiong page
 }
 
 
-function onError(error) {
-    connectingElement.textContent = 'Could not connect to WebSocket server. Please refresh this page to try again!';
-    connectingElement.style.color = 'red';
-}
 
 
 function sendMessage(event) {
@@ -61,9 +55,9 @@ function sendMessage(event) {
             type: 'CHAT'
         };
         stompClient.send("/app/chat.sendMessage", {}, JSON.stringify(chatMessage));
-        messageInput.value = '';
+        messageInput.value = ''; //clearing
     }
-    event.preventDefault();
+    event.preventDefault(); //hz
 }
 
 
@@ -77,21 +71,23 @@ function onMessageReceived(payload) {
         message.content = message.sender + ' joined!';
     } else if (message.type === 'LEAVE') {
         messageElement.classList.add('event-message');
-        message.content = message.sender + ' left!';
+        message.content = message.sender + ' left! -4ort';
     } else {
         messageElement.classList.add('chat-message');
 
         var avatarElement = document.createElement('i');
-        var avatarText = document.createTextNode(message.sender[0]);
-        avatarElement.appendChild(avatarText);
-        avatarElement.style['background-color'] = getAvatarColor(message.sender);
 
-        messageElement.appendChild(avatarElement);
+        var avatarText = document.createTextNode(message.sender[0]);//первая буква
+        avatarElement.appendChild(avatarText); // 1st letter
 
-        var usernameElement = document.createElement('span');
-        var usernameText = document.createTextNode(message.sender);
-        usernameElement.appendChild(usernameText);
-        messageElement.appendChild(usernameElement);
+        avatarElement.style['background-color'] = getAvatarColor(message.sender); //hash color
+
+        messageElement.appendChild(avatarElement); // add avatar
+
+        var usernameElement = document.createElement('span'); //username
+        var usernameText = document.createTextNode(message.sender); //username
+        usernameElement.appendChild(usernameText); //username is putted
+        messageElement.appendChild(usernameElement); //username element putted in message body
     }
 
     var textElement = document.createElement('p');
@@ -107,12 +103,18 @@ function onMessageReceived(payload) {
 
 function getAvatarColor(messageSender) {
     var hash = 0;
-    for (var i = 0; i < messageSender.length; i++) {
-        hash = 31 * hash + messageSender.charCodeAt(i);
+    for (var i=0; i<messageSender.length; i++) {
+        hash = 2*hash+messageSender.charCodeAt(i);
     }
     var index = Math.abs(hash % colors.length);
     return colors[index];
 }
+
+function onError(error) {
+    connectingElement.textContent = 'Could not connect to WebSocket server. Try to refresh';
+    connectingElement.style.color = 'red';
+}
+
 
 usernameForm.addEventListener('submit', connect, true)
 messageForm.addEventListener('submit', sendMessage, true)
